295aa9126baae33d3347dced2d43faaa80285913
Larlf.Wang <larlf.wang@gmail.com>
2015-12-02 15:33:01 +0800

    目录结构调整

--- server/cpp/src/test/utils.cpp
+++ server/cpp/src/test/utils.cpp
@@ -1,25 +1,25 @@
 #include "utils.h"
 
-TEST_F(UtilsTest, test)
+TEST_F(UtilsTest, threadTest1)
 {
-	std::cout << "This is UtilsTest.test()" << std::endl;
-
 	pthread_t pid;
 	pthread_attr_t attr;
 	pthread_attr_init(&attr);
 	pthread_attr_setscope(&attr, PTHREAD_SCOPE_PROCESS);
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
 	pthread_create(&pid, &attr, UtilsTest::thread1, NULL);
-	printf("======================================== ");
+	pthread_create(&pid, &attr, UtilsTest::thread1, NULL);
+	pthread_create(&pid, &attr, UtilsTest::thread1, NULL);
+
 	getchar();
+
 	pthread_attr_destroy(&attr);
 }
 
 
 void* UtilsTest::thread1(void* Param)
 {
-	printf("我是线程！ ");
 	pthread_t myid = pthread_self();
-	printf("线程ID=%d ", myid);
+	std::cout << "Thread ID : " << myid.p << std::endl;
 	return NULL;
 }
