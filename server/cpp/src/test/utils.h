d209d7c4b2f83b34faef043ed1d3eb1eb326a184
Larlf.Wang <larlf.wang@gmail.com>
2015-12-02 15:18:50 +0800

    加入pthread库

---
+++ server/cpp/src/test/utils.h
@@ -0,0 +1,18 @@
+#ifndef MIRAGE_TEST_UTILES
+#define MIRAGE_TEST_UTILES
+
+#ifdef WIN64
+#define HAVE_STRUCT_TIMESPEC
+#endif
+
+#include <iostream>
+#include <gtest/gtest.h>
+#include <pthread.h>
+
+class UtilsTest : public testing::Test
+{
+public:
+	static void* thread1(void* Param);
+};
+
+#endif
